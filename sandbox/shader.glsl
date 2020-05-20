763f83e6fe0dc3c045bd8685b090c89effc70115
NudelErde <37987062+NudelErde@users.noreply.github.com>
2020-05-20 20:53:53 +0200

    fix model loading (#22)

--- sandbox/shader.glsl
+++ sandbox/shader.glsl
@@ -1,6 +1,7 @@
 #tag modelMatrix model
 #tag viewMatrix view
 #tag projectionMatrix projection
+#tag modelMatrix_it modelIT
 #shader vertex
 #version 130
 
@@ -11,9 +12,15 @@ layout(location=2) in vec2 texturePos;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
+uniform mat4 modelIT;
+
+out vec4 v_position;
+out vec3 v_normal;
 
 void main(){
     gl_Position = projection * view * model * position;
+    v_position = model * position;
+    v_normal = mat3(modelIT) * normal;
 }
 
 #shader fragment
@@ -21,6 +28,17 @@ void main(){
 
 out vec4 color;
 
+in vec4 v_position;
+in vec3 v_normal;
+
+uniform vec4 lightSource;
+uniform vec3 lightColor;
+uniform vec3 ambiLight;
+
 void main(){
-    color = vec4(1,0,0,1);
+    vec3 lightDirection = normalize((lightSource - v_position).xyz);
+    float coef = max(dot(v_normal, lightDirection), 0.0);
+    vec3 light = coef * lightColor * (1-ambiLight) + ambiLight;
+    vec3 preColor = vec3(1, 1, 1);
+    color = vec4(preColor * light, 1);
 }
