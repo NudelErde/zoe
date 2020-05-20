e780035692d5bcf2cdb1f70e1047aa916a0476db
NudelErde <flo2013hei@gmail.com>
2020-05-20 19:59:45 +0200

    fix model loading

--- sandbox/Main.cpp
+++ sandbox/Main.cpp
@@ -4,18 +4,20 @@
 
 #include "zoe.h"
 #include "EntryPoint.h"
+#include <cmath>
 
 class Layer : public Zoe::Layer3D {
 public:
 
     void onAttach() override {
-        Zoe::Material material(Zoe::File("shader.glsl"));
+        Zoe::Material material = Zoe::Material(Zoe::File("shader.glsl"));
         Zoe::Model model(Zoe::File("CubeNoSub.obj"));
         std::shared_ptr<Zoe::Camera> camera = std::make_shared<Zoe::Camera>();
-        camera->setPosition({0, 0, 6});
+        camera->setPosition({0, 0, 10});
         camera->setRotation(0, 0, 0);
         camera->setProjectionMatrix(Zoe::perspective(0.1, 100, 90, 16.0 / 9.0));
-        add(Zoe::Object3D(material, model));
+        cube = std::make_shared<Zoe::Object3D>(material, model);
+        add(cube);
         setCamera(camera);
     }
 
@@ -28,9 +30,19 @@ public:
     }
 
     void onRender() override {
-
+        static float position = 0;
+        static float rotation = 0;
+        position += 0.01;
+        cube->getMaterial().setUniform("lightSource", Zoe::vec4({10,10,std::sin(position)*10,1}));
+        cube->getMaterial().setUniform("lightColor", Zoe::vec3({1,1,1}));
+        cube->getMaterial().setUniform("ambiLight", Zoe::vec3({0.2,0.2,0.2}));
+        if(Zoe::Input::isKeyPressed(KEY_SPACE)){
+            rotation += 0.1;
+            cube->getModel().setModelMatrix(Zoe::rotateYZ3D(rotation));
+        }
     }
-
+private:
+    std::shared_ptr<Zoe::Object3D> cube;
 };
 
 class App : public Zoe::Application {
