64d1b9f0d977988f7e3da83daab0a1280371d66b
NudelErde <flo2013hei@gmail.com>
2020-10-20 07:03:01 +0200

    add documentation

--- zoe/src/zoe/render/Font.h
+++ zoe/src/zoe/render/Font.h
@@ -13,47 +13,157 @@ namespace Zoe{
 struct FontData;
 class Texture;
 
+/**
+ * FontCharMetric contains the information on how to draw a specified char.
+ * @see https://www.freetype.org/freetype2/docs/tutorial/step2.html
+ * @see https://www.freetype.org/freetype2/docs/tutorial/metrics.png
+ * @see https://www.freetype.org/freetype2/docs/tutorial/metrics2.png
+ */
 struct DLL_PUBLIC FontCharMetric{
+    /**
+     * This is the width of the glyph image's bounding box. It is independent of the layout direction.
+     */
     long  width;
+    /**
+     * This is the height of the glyph image's bounding box. It is independent of the layout direction.
+     */
     long  height;
 
+    /**
+     * For horizontal text layouts, this is the horizontal distance from the current cursor position to the leftmost border of the glyph image's bounding box.
+     */
     long  horiBearingX;
+
+    /**
+     * For horizontal text layouts, this is the vertical distance from the current cursor position (on the baseline) to the topmost border of the glyph image's bounding box.
+     */
     long  horiBearingY;
+
+    /**
+     * For horizontal text layouts, this is the horizontal distance to increment the pen position when the glyph is drawn as part of a string of text.
+     */
     long  horiAdvance;
 
+    /**
+     * For vertical text layouts, this is the horizontal distance from the current cursor position to the leftmost border of the glyph image's bounding box.
+     */
     long  vertBearingX;
+
+    /**
+     * For vertical text layouts, this is the vertical distance from the current cursor position (on the baseline) to the topmost border of the glyph image's bounding box.
+     */
     long  vertBearingY;
+
+    /**
+     * For vertical text layouts, this is the vertical distance used to increment the pen position when the glyph is drawn as part of a string of text.
+     */
     long  vertAdvance;
 };
 
+/**
+ * Contains the information about the position and size of the char on the generated bitmap.
+ */
 struct DLL_PUBLIC FontCharBitmapInformation{
+    /**
+     * Width on the bitmap.
+     */
     unsigned int width;
+    /**
+     * Height on the bitmap.
+     */
     unsigned int height;
+    /**
+     * X-Coordinate of the top left corner.
+     */
     unsigned int x;
+    /**
+     * Y-Coordinate of the top left corner.
+     */
     unsigned int y;
 };
 
+/**
+ * A Font provides the information to render text in a visable way.
+ */
 class DLL_PUBLIC Font {
 public:
+    /**
+     * Fonts can not be empty so the default constructor is deleted.
+     */
     Font() = delete;
+
+    /**
+     * Create a font with a specified file .ttf file and a specified size.
+     * @param file the specified file
+     * @param size the specified size
+     */
     Font(const File& file, int size);
+
+    /**
+     * Copies the specified font.
+     * @param font the specified font
+     */
     Font(const Font& font);
+
+    /**
+     * Copies the specified font but changes the font size.
+     * @param font the specified font
+     * @param size the new size
+     */
     Font(const Font& font, int size);
 
+    /**
+     * Copies the specified font.
+     * @return a reference to this
+     */
     Font& operator=(const Font&);
 
+    /**
+     * Destructs the font and releases the used resources.
+     */
     ~Font();
 
+    /**
+     * Returns the generated bitmap.
+     * @return the bitmap
+     */
     std::shared_ptr<Texture> getBitmap();
 
+    /**
+     * Get the FontCharMetric for a specified charcode.
+     * @param charcode the specified charcode
+     * @return the FontCharMetric
+     */
     const FontCharMetric& getCharMetric(unsigned long charcode);
+
+    /**
+     * Get the FontCharBitmapInformation for a specified charcode.
+     * @param charcode the specified charcode
+     * @return the FontCharBitmapInformation
+     */
     const FontCharBitmapInformation& getCharBitmapInformation(unsigned long charcode);
 
+    /**
+     * Return the fontsize.
+     * @return the fontsize
+     */
     const int& getSize();
 
+    /**
+     * Get the height of the bitmap texture.
+     * @return the height of the bitmap texture
+     */
     const unsigned int &getTextureHeight();
+
+    /**
+     * Get the width of the bitmap texture.
+     * @return the width of the bitmap texture
+     */
     const unsigned int &getTextureWidth();
 
+    /**
+     * Initializes the font loading library. (FreeType2)
+     */
     static void init();
 
 private:
--- zoe/src/zoe/render/api/ImplPointer.h
+++ zoe/src/zoe/render/api/ImplPointer.h
@@ -9,68 +9,105 @@
 
 #include "../../core/Core.h"
 
-namespace Zoe{
+namespace Zoe {
 
+/**
+ * ImplPointer is used to hold a dynamic allocated instance of the render api implementations.
+ * @tparam T the abstract Type
+ */
 template<typename T>
-class DLL_PUBLIC ImplPointer{
+class DLL_PUBLIC ImplPointer {
 public:
 
-	ImplPointer(): t(nullptr), references(nullptr){}
-	ImplPointer(T* t): t(t){
-		references = new unsigned int();
-		*references = 1;
-	}
-	ImplPointer(const ImplPointer& copy){
-		t = copy.t;
-		references = copy.references;
-		++(*references);
-	}
-	ImplPointer& operator=(const ImplPointer& assign){
-		if(references == assign.references){ //if same object, why copy?
-			return *this;
-		}
+    /**
+     * Creates a null pointing Object.
+     */
+    ImplPointer() : t(nullptr), references(nullptr) {}
 
-		//~ImplPointer()
-		if((*references) == 1){
-			delete references;
-			delete t;
-		}else{
-			--(*references);
-		}
+    /**
+     * Creates an object from an existing pointer.
+     * @param t the specified object
+     */
+    ImplPointer(T* t) : t(t) {
+        references = new unsigned int();
+        *references = 1;
+    }
 
-		//ImplPointer(assign)
-		t = assign.t;
-		references = assign.references;
-		++(*references);
-		return *this;
-	}
-	~ImplPointer(){
-		if((*references) == 1){
-			delete references;
-			delete t;
-		}else{
-			--(*references);
-		}
-	}
-	const T* operator->() const{
-		return t;
-	}
+    /**
+     * Copies the pointer and increases the reference count.
+     * @param rhs the original pointer
+     */
+    ImplPointer(const ImplPointer& rhs) {
+        t = rhs.t;
+        references = rhs.references;
+        ++(*references);
+    }
 
-	const T* operator&() const{
-		return t;
-	}
+    /**
+     * Copies the pointer and increases the reference count.
+     * @param rhs the original pointer
+     * @return this
+     */
+    ImplPointer& operator=(const ImplPointer& rhs) {
+        if (references == rhs.references) {
+            return *this;
+        }
 
-	T* operator->(){
-		return t;
-	}
+        ~ImplPointer();
 
-	T* operator&(){
-		return t;
-	}
+        t = rhs.t;
+        references = rhs.references;
+        ++(*references);
+        return *this;
+    }
+
+    /**
+     * Destructs the pointer and decrements the reference count. If the reference count is zero the stored object is destroyed.
+     */
+    ~ImplPointer() {
+        if ((*references) == 1) {
+            delete references;
+            delete t;
+        } else {
+            --(*references);
+        }
+    }
+
+    /**
+     * Return the underlying pointer.
+     * @return the underlying pointer
+     */
+    const T* operator->() const {
+        return t;
+    }
+
+    /**
+     * Return the underlying pointer.
+     * @return the underlying pointer
+     */
+    const T* operator&() const {
+        return t;
+    }
+
+    /**
+     * Return the underlying pointer.
+     * @return the underlying pointer
+     */
+    T* operator->() {
+        return t;
+    }
+
+    /**
+     * Return the underlying pointer.
+     * @return the underlying pointer
+     */
+    T* operator&() {
+        return t;
+    }
 
 private:
-	T* t;
-	unsigned int* references;
+    T* t;
+    unsigned int* references;
 };
 
 }
